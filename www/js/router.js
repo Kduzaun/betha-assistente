f1a0c90f1a0df1943759afe4bc692f1a7ad35d35
Kduzaun <email@example.com>
2023-09-30 19:47:22 -0300

    Aula Betha Assistente

---
+++ www/js/router.js
@@ -0,0 +1,41 @@
+var app = new Framework7({
+  // App root element
+  el: "#app",
+  // App Name
+  name: "Betha Assistente",
+  // Enable swipe panel
+  panel: {
+    swipe: true,
+  },
+  // Add default routes
+  routes: [
+    {
+      path: "/index/",
+      url: "index.html",
+      on: {
+        pageInit: function (e, page) {},
+      },
+    },
+    {
+      path: "/memory/",
+      url: "memory.html",
+      on: {
+        pageInit: function (e, page) {
+          var searchbar = app.searchbar.create({
+            el: ".searchbar",
+            searchContainer: '.list',
+            searchIn: '.item-title',
+            on: {
+              search(sb, query, previousQuery,){
+                console.log(query, previousQuery);
+              }
+            },
+          })
+        },
+      },
+    },
+  ],
+  // ... other parameters
+})
+
+var mainView = app.views.create(".view-main")
